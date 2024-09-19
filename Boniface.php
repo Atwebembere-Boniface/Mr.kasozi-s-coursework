42c2735c79276cb9254742f3c5a81de9453790cb
Atwebembere-Boniface <atwebembereboniface@gmail.com>
2024-09-19 09:00:07 -0700

    Create Boniface.php
     First  coursework

---
+++ Boniface.php
@@ -0,0 +1,17 @@
+<?php
+//a php script that captures my information.
+// declaring  variables.
+
+$name="Atwebembere Boniface "
+$DOB="25/05/2001"."<br>";
+$address="Ibanda district,western uganda "
+$currentyear="2024";
+$yearofbirth="2001";
+$age =$currentyear-$yearofbirth;
+$remark="Welcome you back home, Boniface!, we have been missing you!!";
+
+echo "My name is ".$name ."<br>";
+echo "I come from ".$address ."<br>";
+echo "I was born on ".$DOB."<br>";
+echo "We ".$remark ."<br>";
+echo "You have turned ".$age "years today,congulatulations!!". "<br>";
